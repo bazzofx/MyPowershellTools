697709939a47ea4f3159cd475cb201c312bc013f
BazzoFX <bazzofx@outlook.com>
2023-01-11 02:57:16 +0000

    Update to CheckSuspicious Folder

--- CheckSuspiciousProcess/ChecksSuspiciousProcess.ps1
+++ CheckSuspiciousProcess/ChecksSuspiciousProcess.ps1
@@ -39,7 +39,8 @@ Start-Sleep 2
     $id = $x.OwningProcess
     $state = $x.State
             
-            if($x.OwningProcess-eq 4){
+            if($id-eq 4 -or $id -eq 16800 -or $id -eq 988 -or $id -eq 5404 -or`
+            $id -eq 2224 -or $id -eq 84 -or $id -eq 916 -or $id -eq 1008){
             $row = New-Object Object
             Write-Host "Windows Process Safe[id:4]" -ForegroundColor Green
             $row | Add-Member -MemberType NoteProperty -Name "ProcessName" -Value $name
@@ -47,12 +48,26 @@ Start-Sleep 2
             $row | Add-Member -MemberType NoteProperty -Name "State"  -Value $state
             $row | Add-Member -MemberType NoteProperty -Name "SecurityRisk"  -Value "Windows Essential"
             }
-            else {
+            elseif ($id -eq 5828){
             $row = New-Object Object
             $row | Add-Member -MemberType NoteProperty -Name "ProcessName" -Value $name
             $row | Add-Member -MemberType NoteProperty -Name "OwningProcess" -Value $id
             $row | Add-Member -MemberType NoteProperty -Name "State"  -Value $state
-            $row | Add-Member -MemberType NoteProperty -Name "SecurityRisk"  -Value "Something else"
+            $row | Add-Member -MemberType NoteProperty -Name "SecurityRisk"  -Value "Intel Service"
+            }
+            elseif($id -eq 16216 -or $id -eq 1868 -or $id -eq 1724 ){
+            $row = New-Object Object
+            $row | Add-Member -MemberType NoteProperty -Name "ProcessName" -Value $name
+            $row | Add-Member -MemberType NoteProperty -Name "OwningProcess" -Value $id
+            $row | Add-Member -MemberType NoteProperty -Name "State"  -Value $state
+            $row | Add-Member -MemberType NoteProperty -Name "SecurityRisk"  -Value "Web Root Services"            
+            }
+            else{
+            $row = New-Object Object
+            $row | Add-Member -MemberType NoteProperty -Name "ProcessName" -Value $name
+            $row | Add-Member -MemberType NoteProperty -Name "OwningProcess" -Value $id
+            $row | Add-Member -MemberType NoteProperty -Name "State"  -Value $state
+            $row | Add-Member -MemberType NoteProperty -Name "SecurityRisk"  -Value "Unknown" 
             }
 
                     
