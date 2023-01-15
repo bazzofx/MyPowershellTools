34816017f1052e3aaa72a5d1d8b84ac185cd16ba
BazzoFX <bazzofx@outlook.com>
2023-01-15 02:51:51 +0000

    SearchString Outlook

---
+++ RedTeam/searchStringOutlook.ps1
@@ -0,0 +1,9 @@
+﻿$Outlook = New-Object -ComObject Outlook.Application
+$namespace = $Outlook.GetNameSpace("MAPI")
+$inbox = $namespace.Folders
+
+$searchString = "example string"
+$Term = 'pass' 
+$emails = $inbox.Items.Restrict($Term)
+
+$emails.Results
